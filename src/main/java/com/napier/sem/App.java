b3db1efc55127d1ca40ae07a5f24f4e97446e855
Adrian Załubski <40690829@live.napier.ac.uk>
2025-10-06 18:27:48 +0100

    Add call to connect/disconnect to main()

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,11 @@
 package com.napier.sem;
 
+import com.napier.sem.config.DatabaseConnection;
+
 public class App {
     public static void main(String[] args) {
+        DatabaseConnection.connect();
 
+        DatabaseConnection.disconnect();
     }
 }
