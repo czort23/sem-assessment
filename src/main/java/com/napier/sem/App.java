7d33e3316ea1616bb5fc6bb581d9db3e3c2c9dfe
Adrian Załubski <40690829@live.napier.ac.uk>
2025-10-13 19:14:55 +0100

    Add run menu in main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,14 @@
 package com.napier.sem;
 
 import com.napier.sem.config.DatabaseConnection;
+import com.napier.sem.ui.MenuSystem;
 
 public class App {
     public static void main(String[] args) {
         DatabaseConnection.connect();
+
+        MenuSystem menuSystem = new MenuSystem();
+        menuSystem.start();
 
         DatabaseConnection.disconnect();
     }
