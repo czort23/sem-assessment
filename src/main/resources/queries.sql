69b418018a7632c8c932cc8855eb21fe2c046d3b
Adrian Załubski <40690829@live.napier.ac.uk>
2025-10-28 16:19:19 +0000

    Revert "Merge pull request #45 from czort23/feature/population-reports"
    This reverts commit 02a9f90441638d8c01df6e22b91d839f6e590d50, reversing
    changes made to 778e1401d903b2d6ae39446a35c25cf8812a0943.

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -24,119 +24,12 @@ INSTRUCTIONS:
 4. To retrieve a query in code:
         String sql = QueryLoader.get("all_countries_desc");
 ===============================================================================
-*/
-/*
-POPULATION REPORTS
-*/
-/*
---Show each continent, total population in each continent,  population living in cities,
-and  population not living in cities  in each continent, ordered by total population (largest to smallest)
-name: continent_population_summary
-*/
-
-
-SELECT
-    co.Continent,
-    SUM(co.Population) AS total_population,
-    SUM(ci.city_population) AS city_population,
-    (SUM(co.Population) - SUM(ci.city_population)) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT
-        CountryCode,
-        SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci
-                   ON co.Code = ci.CountryCode
-GROUP BY co.Continent
-ORDER BY total_population DESC;
-
-
--- Show each Region, total population in eaach region, people living in cities, and people not living in cities
--- for each region, ordered by total population (largest to smallest)
--- name: region_population_summary
-SELECT
-    co.Region,
-    SUM(co.Population) AS total_population,
-    SUM(ci.city_population) AS city_population,
-    (SUM(co.Population) - SUM(ci.city_population)) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT CountryCode, SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci ON co.Code = ci.CountryCode
-GROUP BY co.Region
-ORDER BY total_population DESC;
-
--- Show each Country, total population, people living in cities, and people not living in cities
--- for each country, ordered by total population (largest to smallest)
--- name: country_population_summary
-SELECT
-    co.Name AS Country,
-    SUM(co.Population) AS total_population,
-    SUM(ci.city_population) AS city_population,
-    (SUM(co.Population) - SUM(ci.city_population)) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT CountryCode, SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci ON co.Code = ci.CountryCode
-GROUP BY co.Name
-ORDER BY total_population DESC;
-
--- Show population for a specific continent provided by the user
--- name: population_by_continent
-SELECT
-    co.Continent,
-    SUM(co.Population) AS total_population,
-    SUM(ci.city_population) AS city_population,
-    (SUM(co.Population) - SUM(ci.city_population)) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT CountryCode, SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci ON co.Code = ci.CountryCode
-WHERE co.Continent = ?
-GROUP BY co.Continent;
-
--- Show population for a specific region provided by the user
--- name: population_by_region
-SELECT
-    co.Region,
-    SUM(co.Population) AS total_population,
-    SUM(ci.city_population) AS city_population,
-    (SUM(co.Population) - SUM(ci.city_population)) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT CountryCode, SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci ON co.Code = ci.CountryCode
-WHERE co.Region = ?
-GROUP BY co.Region;
-
--- Show population for a specific country provided by the user
--- name: population_by_country
-SELECT
-    co.Name AS Country,
-    co.Population AS total_population,
-    ci.city_population,
-    (co.Population - ci.city_population) AS non_city_population
-FROM country AS co
-         LEFT JOIN (
-    SELECT CountryCode, SUM(Population) AS city_population
-    FROM city
-    GROUP BY CountryCode
-) AS ci ON co.Code = ci.CountryCode
-WHERE co.Name = ?;
  */
 
 /*
+===============================================================================
  COUNTRY REPORTS
+===============================================================================
  */
 
 -- All the countries in the world organised by largest population to smallest.
@@ -189,7 +82,9 @@ ORDER BY c.Population DESC
 LIMIT ?;
 
 /*
+===============================================================================
  CITY REPORTS
+===============================================================================
  */
 
 -- All the cities in the world organised by largest population to smallest.
@@ -276,7 +171,9 @@ ORDER BY ci.Population DESC
 LIMIT ?;
 
 /*
+===============================================================================
  LANGUAGE REPORTS
+===============================================================================
  */
 
 -- Number of people who speak Chinese, English, Hindi, Spanish, Arabic (sorted from greatest to smallest, with world %)
