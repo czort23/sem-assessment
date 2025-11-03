7d4b72c7f3fb54182f2d6b4099cc0ecad81f9b8c
Adrian Załubski <40690829@live.napier.ac.uk>
2025-11-03 17:23:44 +0000

    Add report headers to reports

--- src/main/java/com/napier/sem/helper/OutputHelper.java
+++ src/main/java/com/napier/sem/helper/OutputHelper.java
@@ -15,6 +15,36 @@ public class OutputHelper {
             System.out.println("No results found.");
             return;
         }
+
+        // Detect class type
+        Class<?> clazz = list.get(0).getClass();
+
+        printHeader(clazz);
+
         list.forEach(System.out::println);
+    }
+
+    /**
+     * Outputs the correct report header based on the class being passed.
+     * @param clazz The class which info is being output.
+     */
+    private static void printHeader(Class<?> clazz) {
+        // Choose class
+        String header = switch (clazz.getSimpleName()) {
+            case "CapitalCity" -> String.format("\n%-35s %-40s %-15s",
+                    "Name:", "Country:", "Population:");
+            case "City" -> String.format("\n%-35s %-40s %-25s %-15s",
+                    "Name:", "Country:", "District:", "Population:");
+            case "Country" -> String.format("\n%-10s %-40s %-20s %-30s %-15s %-20s",
+                    "Code:", "Name:", "Continent:", "Region:", "Population:", "Capital:");
+            case "LanguageReport" -> String.format("\n%-20s %-20s %-20s",
+                    "Language:", "Speakers:", "World Percentage:");
+            case "PopulationReport" -> String.format("\n%-40s %-15s %-15s %-10s %-15s %-10s",
+                    "Name:", "Total Pop.:", "City Pop.:", "City %:", "Non-City Pop.:", "Non-City %:");
+            default -> "";
+        };
+
+        System.out.println(header);
+        System.out.println("----------------------");
     }
 }
