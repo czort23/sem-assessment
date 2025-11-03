5dbd1f437a5a171087ba1a2cdf0b982d0d0c58db
Adrian Załubski <40690829@live.napier.ac.uk>
2025-11-03 21:48:12 +0000

    Refactor to run unit tests and docker simultaneously Refactor App class and docker run settings to skip the menu when run as part of GitHub Actions to deal with user input issue

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,8 +7,11 @@ public class App {
     public static void main(String[] args) {
         DatabaseConnection.connect();
 
-      // MenuSystem menuSystem = new MenuSystem();
-      //  menuSystem.start();
+        if (args.length > 0 && args[0].equals("--no-menu")) {
+            System.out.println("Skipping interactive menu...");
+        } else {
+            new MenuSystem().start();
+        }
 
         DatabaseConnection.disconnect();
     }
